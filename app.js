8866d4db6c8467a141803e1b0de6d737c1f6cc76
mariapesm <youandmecanyousee@hotmail.com>
2018-12-06 17:28:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('/wordpress-extensions-browsers/');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+{
+  "name": "Node.js Sample",
+  "description": "A barebones Node.js app using Express 4",
+  "repository": "https://github.com/heroku/node-js-sample",
+  "logo": "https://rawgit.com/heroku/node-js-sample/master/public/node.svg",
+  "keywords": ["node", "express", "static"]
+}
