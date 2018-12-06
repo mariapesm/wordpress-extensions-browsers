51e754129dcb41dcf037b0cf07a6f7387c7db955
mariapesm <youandmecanyousee@hotmail.com>
2018-12-06 17:56:42 +0200

    Delete web.js

--- web.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var https = require('https');
-var http = require('http');
-app.get('/', function (req, res) {
- res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(process.env.PORT || 3000);
