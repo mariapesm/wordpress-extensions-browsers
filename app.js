3eff1908657ce50e739e5c43762c3728041284c3
mariapesm <youandmecanyousee@hotmail.com>
2018-12-06 17:13:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('mariapesm.github.io/wordpress-extensions-browsers/');
+res.end('/wordpress-extensions-browsers/');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
